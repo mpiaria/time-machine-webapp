aea1e991ad1f17c91b3e5b2c5931b59613f27556
Michael Iaria <8315738+mpiaria@users.noreply.github.com>
2021-09-25 20:29:35 -0500

    Chapter02.SimpleFunctions
    Added minmax

--- Chapter02/src/Chapter02/SimpleFunctions/Example.hs
+++ Chapter02/src/Chapter02/SimpleFunctions/Example.hs
@@ -1,7 +1,8 @@
 module Chapter02.SimpleFunctions.Example
   ( firstOrEmpty,
     (+++),
-    reverse2
+    reverse2,
+    minmax
   )
 where
 
@@ -22,3 +23,14 @@ reverse2 list =
   if null list
     then list
     else reverse2 (tail list) +++ [head list]
+
+minmax :: Ord a => [a] -> (a, a)
+minmax list =
+  let h = head list
+   in if null (tail list)
+        then (h, h)
+        else (if h < low then h else low, if h > high then h else high)
+  where
+    low_high = minmax (tail list)
+    low = fst low_high
+    high = snd low_high
